1e606de239468926b9287582e588899258374fd7
ilhamriz <milhamrizky7@gmail.com>
2025-03-02 17:07:38 +0700

    add title and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import { useTheme } from "@/providers/ThemeProvider";
 import { Switch } from "./ui/switch";
 import { IconMoon, IconSun } from "./Icons";
+import { useEffect } from "react";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    const favicon = document.getElementById("favicon");
+    favicon.setAttribute("href", `/logo-${theme}.svg`);
+  }, [theme]);
 
   return (
     <div className="flex justify-center items-center gap-2 pt-4">
